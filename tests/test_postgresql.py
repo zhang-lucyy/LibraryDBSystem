1ccf3da822bedc37369b1494edbfaf60f0f8cdd4
Lucy Zhang <lz3744@rit.edu>
2022-08-27 13:33:32 -0400

    changed test_can_connect

--- tests/test_postgresql.py
+++ tests/test_postgresql.py
@@ -1,14 +1,11 @@
 import unittest
-from src.swen344_db_utils import connect
+from src.swen344_db_utils import exec_get_one
 
 class TestPostgreSQL(unittest.TestCase):
 
     def test_can_connect(self):
-        conn = connect()
-        cur = conn.cursor()
-        cur.execute('SELECT VERSION()')
-        self.assertTrue(cur.fetchone()[0].startswith('PostgreSQL'))
-        conn.close()
+        result = exec_get_one('SELECT VERSION()')
+        self.assertTrue(result[0].startswith('PostgreSQL'))
 
 if __name__ == '__main__':
     unittest.main()
