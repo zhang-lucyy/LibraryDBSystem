b4c6209856b622523b13afdbfdcaccfc971ed03c
Lucy Zhang <lz3744@rit.edu>
2022-09-13 21:09:23 -0400

    CREATE tables

--- src/library_schema.sql
+++ src/library_schema.sql
@@ -1,12 +1,14 @@
-CREATE SCHEMA library;
+DROP TABLE IF EXISTS checkout;
+DROP TABLE IF EXISTS inventory;
+DROP TABLE IF EXISTS users;
 
-CREATE TABLE library.users(
+CREATE TABLE users(
     id SERIAL NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
     contact_info TEXT NOT NULL 
 );
 
-CREATE TABLE library.inventory(
+CREATE TABLE inventory(
     book_id SERIAL NOT NULL PRIMARY KEY,
     title TEXT NOT NULL,
     book_type TEXT NOT NULL,
@@ -15,9 +17,9 @@ CREATE TABLE library.inventory(
     copies INTEGER DEFAULT 0 NOT NULL
 );
 
-CREATE TABLE library.checkout(
+CREATE TABLE checkout(
     checked_out INTEGER,
     user_id INTEGER,
-    FOREIGN KEY(user_id) REFERENCES library.users(id),
-    FOREIGN KEY(book_checked_out_id) REFERENCES library.inventory(book_id)
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(checked_out) REFERENCES inventory(book_id)
 );
