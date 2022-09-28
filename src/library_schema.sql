5d0451e9696eb4c4e0cdf92db6bc9afc318bda2b
Lucy Zhang <lz3744@rit.edu>
2022-09-28 16:59:43 -0400

    added table for returning + books have 1 line summary

--- src/library_schema.sql
+++ src/library_schema.sql
@@ -14,6 +14,7 @@ CREATE TABLE inventory(
     book_type TEXT NOT NULL,
     author TEXT NOT NULL,
     publish_date INTEGER NOT NULL,
+    summary TEXT DEFAULT '' NOT NULL,
     copies INTEGER DEFAULT 0 NOT NULL
 );
 
@@ -24,6 +25,14 @@ CREATE TABLE checkout(
     FOREIGN KEY(checked_out) REFERENCES inventory(book_id)
 );
 
+CREATE TABLE return(
+    return_book_id INTEGER,
+    return_book_date DATE,
+    user_id INTEGER,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(return_book_id) REFERENCES inventory(book_id)
+);
+
 INSERT INTO users(name, contact_info) VALUES
     ('Ada Lovelace', 'ALovelace@gmail.com'),
     ('Mary Shelley', 'MShelley@gmail.com'),
@@ -31,14 +40,21 @@ INSERT INTO users(name, contact_info) VALUES
     ('Art Garfunkel', 'AGarfunkel@gmail.com');
 
 INSERT INTO inventory(title, book_type, author, publish_date, copies) VALUES
-    ('Figuring', 'Non-fiction', 'Maria Popova', 2019, 5),
-    ('In Defence of Witches', 'Non-fiction', 'Mona Chollet', 2022, 3),
-    ('Scary Smart', 'Non-fiction', 'Mo Gawdat', 2021, 7),
-    ('The Princess Spy', 'Non-fiction', 'Larry Loftis', 2022, 2),
-    ('The Dead Romantics', 'Fiction', 'Ashley Poston', 2022, 6),
-    ('The Lord of the Rings', 'Fiction', 'J.R.R. Tolkien', 1954, 9),
-    ('The Lightning Thief', 'Fiction', 'Rick Riordan', 2005, 4),
-    ('To Kill a Mockingbird', 'Fiction', 'Harper Lee', 1960, 1);
+    ('Figuring', 'Non-fiction', 'Maria Popova', 2019, 'Explores the complexities
+        of love and the human search for truth and meaning', 5),
+    ('In Defence of Witches', 'Non-fiction', 'Mona Chollet', 2022, 'Explores how
+        women who assert their powers are too often seen as a threat to men and society', 3),
+    ('Scary Smart', 'Non-fiction', 'Mo Gawdat', 2021, 'Explores the future of artificial intelligence', 7),
+    ('The Princess Spy', 'Non-fiction', 'Larry Loftis', 2022, 'Follows the hidden history 
+        of an ordinary American girl who became one of the most daring WWII spies', 2),
+    ('The Dead Romantics', 'Fiction', 'Ashley Poston', 2022,
+        'The main character is a ghostwriter for a romance novelist' 6),
+    ('The Lord of the Rings', 'Fiction', 'J.R.R. Tolkien', 1954, 
+        'A group of heroes set forth to save their world', 9),
+    ('The Lightning Thief', 'Fiction', 'Rick Riordan', 2005, 
+        'A 12 year-old boy who learns that his true father is Poseidon', 4),
+    ('To Kill a Mockingbird', 'Fiction', 'Harper Lee', 1960,
+        'Chronicles the childhood of Scout and Jem Finch',1);
 
 INSERT INTO checkout(user_id, checked_out)VALUES
     --Ada checked out "In Defence of Witches"
