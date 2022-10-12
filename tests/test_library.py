c7d5869713f99abe744a64f675fb86c4683d0000
Lucy Zhang <lz3744@rit.edu>
2022-10-12 18:28:06 -0400

    reserve book test fail and success

--- tests/test_library.py
+++ tests/test_library.py
@@ -14,7 +14,7 @@ class TestLibrary(unittest.TestCase):
         print("\nNumber of rows in users table:", actual)
 
     def test_verify_inventory_rows(self):
-        expected = 8
+        expected = 9
         actual = get_nonfiction_books().__len__() + get_fiction_books().__len__()
         self.assertEqual(expected, actual, "incorrect count of rows for inventory table")
         print("\nNumber of rows in inventory table:", actual)
@@ -57,7 +57,7 @@ class TestLibrary(unittest.TestCase):
         print("\nAll non-fiction books:", actual)
 
     def test_get_fiction_books(self):
-        expected = 4
+        expected = 5
         actual = get_fiction_books()
         self.assertEqual(expected, actual.__len__(), "not all expected fiction books are listed")
         print("\nAll fiction books:", actual)
@@ -84,7 +84,28 @@ class TestLibrary(unittest.TestCase):
         self.assertEqual(expected1, actual1, "did not create an account for Christopher Marlowe")
         self.assertEqual(expected2, actual2, "did not create an account for Francis Bacon")
         print("\nChristopher Marlowe and Francis Bacon each sign up for a new account")
-    
+
+    def test_reserve_book_fail(self):
+        user_id = get_user_id('Jackie Gleason')
+        book_id = get_book_id('Figuring')
+
+        with self.assertRaises(Exception) as cannot_reserve:
+            reserve_book(book_id, user_id)
+            self.assertTrue('Copies are still available' in cannot_reserve.exception)
+        print("\nJackie Gleason reserves a book incorrectly")
+
+    def test_reserve_book_success(self):
+        user_id = get_user_id('Jackie Gleason')
+        book_id = get_book_id('The Lord of the Rings')
+        reserve_book(book_id, user_id)
+        expected = [(6, 3)]
+        actual = exec_get_all("""
+            SELECT * From reserve WHERE user_id = %(user_id)s""",
+            {'user_id': user_id})
+
+        self.assertEqual(expected, actual, "did not successfully reserve a book")
+        print("\nJackie Gleason successfully reserves a book")
+
     def test_delete_account(self):
         return_book(3, 2, '2020-09-10')
         delete_account('Mary Shelley')
