42883f7d385881d47d88c3ef166d2032cac51bdc
Lucy Zhang <lz3744@rit.edu>
2022-11-07 18:01:54 -0500

    added comments

--- src/library.py
+++ src/library.py
@@ -10,6 +10,8 @@ def rebuild_tables():
 
 '''
 Returns all of the users in the system.
+Returns:
+    (list): A list of users (tuples)
 '''
 def get_all_users():
     return exec_get_all("SELECT id, name FROM users ORDER BY id ASC")
@@ -18,6 +20,8 @@ def get_all_users():
 Returns a user's id given their name.
 Parameter: 
     name(str): A user's first and last name. 
+Returns:
+    (int): A user's id number in the system.
 '''
 def get_user_id(name):
     return exec_get_one("SELECT id FROM users WHERE users.name = %(name)s", {'name': name})
@@ -26,6 +30,8 @@ def get_user_id(name):
 Returns a user's contact info given their user id.
 Parameter:
     id(int): A user id.
+Returns:
+    (str): A user's contact info.
 '''
 def get_user_contact_info(id):
     return exec_get_one("""
@@ -34,6 +40,8 @@ def get_user_contact_info(id):
 
 '''
 Returns all of the books in inventory.
+Returns:
+    (list): A list of books (tuples).
 '''
 def get_all_books():
     return exec_get_all("""
@@ -44,6 +52,8 @@ def get_all_books():
 Returns all the books currently checked out by a user.
 Parameter:
     id(int): A user id.
+Returns:
+    (list): A list of books (tuples).
 '''
 def get_user_books(id):
     return exec_get_all("""
@@ -52,9 +62,11 @@ def get_user_books(id):
         WHERE checkout.user_id = '%(id)s' ORDER BY title ASC""",{'id': id})
 
 '''
-Returns a list of all books checked out (sorted by book type / author).
+Returns all books checked out (sorted by book type / author).
 For each book, it says who checked it out and when; return date (or if not returned);
 remaining copies.
+Returns:
+    (list): A list of books (tuples).
 '''
 def get_checked_out_books():
     return exec_get_all("""
@@ -67,6 +79,8 @@ def get_checked_out_books():
 
 '''
 Returns all of the nonfiction books in the inventory.
+Returns:
+    (list): A list of books (tuples).
 '''
 def get_nonfiction_books():
     return exec_get_all("""
@@ -76,6 +90,8 @@ def get_nonfiction_books():
 
 '''
 Returns all of the fiction books in the inventory.
+Returns:
+    (list): A list of books (tuples).
 '''
 def get_fiction_books():
     return exec_get_all("""
@@ -87,6 +103,8 @@ def get_fiction_books():
 Returns all the books in the inventory that are written by a particular author.
 Parameter:
     author(str): Author's name.
+Returns:
+    (list): A list of books (tuples).
 '''
 def search_by_author(author):
     return exec_get_all("""
@@ -98,6 +116,8 @@ def search_by_author(author):
 Returns all of the books in the inventory that match the given title.
 Parameter:
     title(str): A book title.
+Returns:
+    (list): A list of books (tuples).
 '''
 def search_by_title(title):
     return exec_get_all("""
@@ -109,6 +129,8 @@ def search_by_title(title):
 Returns the number of book copies left given the book id.
 Parameter:
     book_id(int): A book's id.
+Returns:
+    (int): Number of book copies.
 ''' 
 def get_book_copies(book_id):
     return exec_get_one("""
@@ -119,6 +141,8 @@ def get_book_copies(book_id):
 Returns the corresponding book id given the book title.
 Parameter:
     title(str): A book title.
+Returns:
+    (int): A book's id.
 '''
 def get_book_id(title):
     return exec_get_one("""
@@ -132,7 +156,7 @@ Parameters:
     contact_info(str): The user's email.
 '''
 def create_account(name, contact_info):
-    return exec_commit("""
+    exec_commit("""
         INSERT INTO users (name, contact_info) VALUES (%(name)s, %(contact_info)s)
         """, {'name': name, 'contact_info': contact_info})
 
@@ -142,7 +166,7 @@ Parameter:
     name(str): The user's first and last name.
 '''
 def delete_account(name):
-    return exec_commit("""
+    exec_commit("""
         DELETE FROM users WHERE users.name = %(name)s""", {'name': name})
 
 '''
@@ -157,7 +181,7 @@ def checkout_book(book_id, user_id, check_out_date):
         UPDATE inventory SET copies = (copies - 1)
         WHERE book_id = %(book_id)s""", {'book_id': book_id})
 
-    return exec_commit("""
+    exec_commit("""
         INSERT INTO checkout (book_id, user_id, check_out_date)
         VALUES (%(book_id)s, %(user_id)s, %(check_out_date)s)""",
         {'book_id': book_id, 'user_id': user_id, 'check_out_date': check_out_date})
@@ -174,7 +198,7 @@ def return_book(book_id, user_id, return_date):
         UPDATE inventory SET copies = (copies + 1)
         WHERE book_id = %(book_id)s""", {'book_id': book_id})
     
-    return exec_commit("""
+    exec_commit("""
         UPDATE checkout SET return_date = %(return_date)s
         WHERE book_id = %(book_id)s
         AND user_id = %(user_id)s""",
@@ -190,7 +214,7 @@ Parameters:
 def reserve_book(reserve_book_id, user_id):
     book_copies = get_book_copies(reserve_book_id)
     if (book_copies[0] == 0):
-        return exec_commit("""
+        exec_commit("""
             INSERT INTO reserve(reserve_book_id, user_id)
             VALUES (%(reserve_book_id)s, %(user_id)s)""",
             {'reserve_book_id': reserve_book_id, 'user_id': user_id})
