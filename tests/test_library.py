97ed5ef67ad54bdcc217c0079cad2b80839a9d71
Lucy Zhang <lz3744@rit.edu>
2022-11-25 01:22:49 -0500

    added new book to library test

--- tests/test_library.py
+++ tests/test_library.py
@@ -11,28 +11,28 @@ class TestLibrary(unittest.TestCase):
     def test_verify_users_rows(self):
         expected = 4
         actual = get_all_users().__len__()
-        self.assertEqual(expected, actual, "incorrect count of rows for users table")
-        print("\nNumber of rows in users table:", actual)
+        self.assertEqual(expected, actual, 'incorrect count of rows for users table')
+        print('\nNumber of rows in users table:', actual)
 
     def test_verify_inventory_rows(self):
         expected = 21
         actual = get_nonfiction_books().__len__() + get_fiction_books().__len__()
-        self.assertEqual(expected, actual, "incorrect count of rows for inventory table")
-        print("\nNumber of rows in inventory table:", actual)
+        self.assertEqual(expected, actual, 'incorrect count of rows for inventory table')
+        print('\nNumber of rows in inventory table:', actual)
 
     def test_verify_library_rows(self):
         expected = 4
         actual = exec_get_all("""
             SELECT * FROM libraries
         """)
-        self.assertEqual(expected, actual.__len__(), "not all libraries are listed")
-        print("\nLibraries:", actual)
+        self.assertEqual(expected, actual.__len__(), 'not all libraries are listed')
+        print('\nLibraries:', actual)
 
     def test_verify_checkout_rows(self):
         expected = 5
         actual = get_checked_out_books().__len__()
-        self.assertEqual(expected, actual, "incorrect count of rows for checkout table")
-        print("\nNumber of rows in checkout table:", actual)
+        self.assertEqual(expected, actual, 'incorrect count of rows for checkout table')
+        print('\nNumber of rows in checkout table:', actual)
 
     def test_get_user_contact_info(self):
         expected = 'ALovelace@gmail.com'
@@ -44,49 +44,49 @@ class TestLibrary(unittest.TestCase):
         rebuild_tables()
         expected = []
         actual = get_user_books(4)
-        self.assertEqual(expected, actual, "expected empty list from art")
+        self.assertEqual(expected, actual, 'expected empty list from Art')
         print("\nArt's checked out books:", actual)
 
     def test_get_user_books_gleason(self):
         expected = 2
         actual = get_user_books(3)
-        self.assertEqual(expected, actual.__len__(), "expected books The Lightning Thief and To Kill a Mockingbird are checked out by Gleason")
+        self.assertEqual(expected, actual.__len__(), 'expected books The Lightning Thief and To Kill a Mockingbird are checked out by Gleason')
         print("\nJackie Gleason's checked out books in alphabetical order:", actual)
 
     def test_get_nonfiction_books(self):
         expected = 4
         actual = get_nonfiction_books()
-        self.assertEqual(expected, actual.__len__(), "not all expected non-fiction books are listed")
-        print("\nAll non-fiction books:", actual)
+        self.assertEqual(expected, actual.__len__(), 'not all expected non-fiction books are listed')
+        print('\nAll non-fiction books:', actual)
 
     def test_get_fiction_books(self):
-        expected = 5
+        expected = 6
         actual = get_fiction_books()
-        self.assertEqual(expected, actual.__len__(), "not all expected fiction books are listed")
-        print("\nAll fiction books:", actual)
+        self.assertEqual(expected, actual.__len__(), 'not all expected fiction books are listed')
+        print('\nAll fiction books:', actual)
 
     def test_search_by_author(self):
         expected = ('To Kill a Mockingbird', 'Fiction', 'Harper Lee', 1960, 1)
         actual = search_by_author('Harper Lee')[0]
-        self.assertEqual(expected, actual, "not all books by Harper Lee are listed")
-        print("\nBooks in inventory by Harper Lee:", actual)
+        self.assertEqual(expected, actual, 'not all books by Harper Lee are listed')
+        print('\nBooks in inventory by Harper Lee:', actual)
 
     def test_create_account(self):
         create_account('Christopher Marlowe', 'CMarlowe@gmail.com')
-        expected1 = [(5, 'Christopher Marlowe', 'CMarlowe@gmail.com', False)]
+        expected1 = [(5, 'Christopher Marlowe', 'CMarlowe@gmail.com')]
         actual1 = exec_get_all("""
-            SELECT * From users WHERE name = \'Christopher Marlowe\'
+            SELECT * FROM users WHERE name = \'Christopher Marlowe\'
         """)
 
         create_account('Francis Bacon', 'FBacon@gmail.com')
-        expected2 = [(6, 'Francis Bacon', 'FBacon@gmail.com', False)]
+        expected2 = [(6, 'Francis Bacon', 'FBacon@gmail.com')]
         actual2 = exec_get_all("""
-            SELECT * From users WHERE name = \'Francis Bacon\'
+            SELECT * FROM users WHERE name = \'Francis Bacon\'
         """)
 
-        self.assertEqual(expected1, actual1, "did not create an account for Christopher Marlowe")
-        self.assertEqual(expected2, actual2, "did not create an account for Francis Bacon")
-        print("\nChristopher Marlowe and Francis Bacon each sign up for a new account")
+        self.assertEqual(expected1, actual1, 'did not create an account for Christopher Marlowe')
+        self.assertEqual(expected2, actual2, 'did not create an account for Francis Bacon')
+        print('\nChristopher Marlowe and Francis Bacon each sign up for a new account')
 
     def test_reserve_book_fail(self):
         user_id = get_user_id('Jackie Gleason')
@@ -95,7 +95,7 @@ class TestLibrary(unittest.TestCase):
         with self.assertRaises(Exception) as cannot_reserve:
             reserve_book(1, book_id, user_id)
             self.assertTrue('Copies are still available' in cannot_reserve.exception)
-        print("\nJackie Gleason reserves a book incorrectly")
+        print('\nJackie Gleason reserves a book incorrectly')
 
     def test_reserve_book_success(self):
         user_id = get_user_id('Jackie Gleason')
@@ -103,23 +103,23 @@ class TestLibrary(unittest.TestCase):
         reserve_book(1, book_id, user_id)
         expected = [(1, 6, 3)]
         actual = exec_get_all("""
-            SELECT * From reserve WHERE user_id = %(user_id)s""",
+            SELECT * FROM reserve WHERE user_id = %(user_id)s""",
             {'user_id': user_id})
 
-        self.assertEqual(expected, actual, "did not successfully reserve a book")
-        print("\nJackie Gleason successfully reserves a book")
+        self.assertEqual(expected, actual, 'did not successfully reserve a book')
+        print('\nJackie Gleason successfully reserves a book')
 
     def test_return_book(self):
         user_id = get_user_id('Art Garfunkel')
-        book_id = get_book_id("Frankenstein")
+        book_id = get_book_id('Frankenstein')
         checkout_book(1, book_id, user_id, '2020-09-10')
         return_book(1, book_id, user_id, '2020-09-13')
-        expected = [(1, 9, 4, datetime.date(2020, 9, 10), datetime.date(2020, 9, 13))]
+        expected = [(1, 9, 4, datetime.date(2020, 9, 10), None, datetime.date(2020, 9, 13))]
         actual = exec_get_all("""
-            SELECT * From checkout WHERE user_id = %(user_id)s""",
+            SELECT * FROM checkout WHERE user_id = %(user_id)s""",
             {'user_id': user_id})
 
-        self.assertEqual(expected, actual, "Art did not return the book")
+        self.assertEqual(expected, actual, 'Art did not return the book')
         print('\nArt Garfunkel returns a copy of “Frankenstein” three days after he borrowed it')
 
     def test_delete_account(self):
@@ -127,7 +127,7 @@ class TestLibrary(unittest.TestCase):
         delete_account('Mary Shelley')
         expected = []
         actual = exec_get_all("""
-            SELECT * From users WHERE name = 'Mary Shelley'
+            SELECT * FROM users WHERE name = 'Mary Shelley'
         """)
 
         self.assertEqual(expected, search_by_title('The Last Man'))
@@ -137,12 +137,60 @@ class TestLibrary(unittest.TestCase):
     def test_get_checked_out_books(self):
         expected = 3
         actual = get_checked_out_books()
-        self.assertEqual(expected, actual.__len__(), "not all checked out books are listed")
-        print("\nThe librarian gets a list of all books checked out:", actual)
+
+        self.assertEqual(expected, actual.__len__(), 'not all checked out books are listed')
+        print('\nThe librarian gets a list of all books checked out:', actual)
 
     def test_insert_data_from_csv(self):
         insert_data_from_csv('src/Library.csv')
         expected = 28   # 19 from csv + 9 from schema insert
         actual = get_all_books().__len__()
+
         self.assertEqual(expected, actual, 'something wrong about book data')
-        print("\nAll .csv books loaded successfully into database")
+        print('\nAll .csv books loaded successfully into database')
+
+    def test_checkout(self):
+        user_id = 2
+        book_id = 1
+        checkout_book(3,book_id,user_id,'2020-09-10')
+        expected = [(3, 1, 2, datetime.date(2020, 9, 10), datetime.date(2020, 9, 24), None)]
+        actual = exec_get_all("""
+            SELECT * FROM checkout WHERE user_id = %(user_id)s
+            AND book_id = %(book_id)s""",
+            {'user_id': user_id, 'book_id': book_id})
+
+        self.assertEqual(expected, actual, 'Pre-assigned due date is not correct')
+        print('\nCheckout works correctly, book has pre-assigned due date')
+    
+    # tests both add_new_book & add_to_library
+    def test_add_new_book_to_library(self):
+        title = 'The Winds of Winter'
+        book_type = 'Fiction'
+        author = 'George R.R. Martin'
+        copies = 4
+        add_new_book(title, book_type, author, copies)
+        book_id = get_book_id(title)
+        expected1 = (book_id[0], title, book_type, author, None, '', copies)
+        actual1 = exec_get_one("""
+            SELECT * FROM inventory
+            WHERE title = %(title)s AND author = %(author)s""",
+            {'title': title, 'author': author})
+        
+        self.assertEqual(expected1, actual1, 'new book is not added to the master inventory')
+        print('\nThe new book, "The Winds of Winter", was successfully added to the master inventory')
+
+        # each library only has 1 copy
+        add_to_library(1, book_id, 1)
+        add_to_library(2, book_id, 1)
+        add_to_library(3, book_id, 1)
+        add_to_library(4, book_id, 1)
+
+        actual2 = exec_get_all("""
+            SELECT * FROM library_stock
+            WHERE book_id = %(book_id)s""",
+            {'book_id': book_id})
+        
+        self.assertEqual(4, actual2.__len__(), "each library should only have 1 copy")
+        print('\nEach library has only one copy of "The Winds of Winter"')
+
+
